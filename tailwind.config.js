2db2cc4fcc269f8d4b6ada716c3685594246fcf7
Victoria Viana <victoriagarcia963@gmail.com>
2024-05-24 23:02:20 -0300

    feature: início de padronização de estilos

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+
+import { gray, green, lime, pink, rose } from 'tailwindcss/colors'
+
 export default {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   content: [],
   theme: {
+    colors: {
+      lime,
+      green,
+      pink,
+      rose,
+      gray
+    },
     extend: {}
   },
   plugins: []
