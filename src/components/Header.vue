cad593cd60710cd04bc57081a4d825f79fec541e
Victoria Viana <victoriagarcia963@gmail.com>
2024-05-25 20:15:09 -0300

    feature: estilização de cabeçalho, hover para itens do menu

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,60 @@
-<script></script>
+<script setup lang="ts">
+const menuList = ['Início', 'Sobre', 'Desenvolvimento', 'Projetos']
+</script>
 
 <template>
-  <header id="header"></header>
+  <header id="header" class="fixed z-10">
+    <div class="flex flex-col justify-start">
+      <h1 id="title" class="text-3xl lg:text-4xl 2xl:text-6xl tracking-wide">Victoria Viana</h1>
+      <p id="subtitle" class="text-sm 2xl:text-base tracking-tighter">
+        Desenvolvedora Web Full-Stack
+      </p>
+
+      <nav id="menu" class="text-sm my-5">
+        <ul class="leading-7">
+          <li v-for="(listName, index) in menuList" :key="index" class="underline-hover mb-1">
+            {{ listName }}
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </header>
 </template>
+
+<style>
+#header {
+  left: calc(var(--pad) * 2);
+  top: calc(var(--pad) * 2);
+}
+
+#menu {
+  font-family: var(--font-secondary);
+}
+
+.underline-hover {
+  padding-bottom: 0.15rem;
+  position: relative;
+}
+
+.underline-hover::before {
+  content: '';
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  width: 0;
+  height: 2px;
+  background-image: linear-gradient(
+    to right,
+    var(--bg-pink-400),
+    var(--bg-tertiary),
+    var(--bg-primary)
+  );
+  transition: width 0.25s ease-out;
+}
+
+.underline-hover:hover::before {
+  width: 50%;
+  height: 2px;
+  background-color: var(--bg-pink-400);
+}
+</style>
--- src/views/FrontPage.vue
+++ src/views/FrontPage.vue
@@ -4,7 +4,7 @@ import Header from '../components/Header.vue'
 </script>
 
 <template>
-  <div id="front-page" class="fixed top-0 left-0 w-full h-full overflow-hidden">
+  <div id="front-page" class="fixed top-0 left-0 w-full h-full overflow-hidden text-lime-700">
     <AnimatedBackground />
     <Header />
   </div>
