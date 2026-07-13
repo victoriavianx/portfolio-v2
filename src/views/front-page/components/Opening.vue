4f81cb0c144f52b53965808edfd4af8bc96535fd
Victoria <victoria@espressolabs.com.br>
2026-07-13 20:23:45 -0300

    style: aumento da largura do texto

--- src/views/front-page/components/Opening.vue
+++ src/views/front-page/components/Opening.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div id="about" class="fixed z-10 w-full max-w-64 sm:max-w-80 overflow-y-scroll max-h-80">
-    <p class="tracking-tight text-sm lg:text-base text-right">
+  <div id="about" class="fixed z-10 w-full max-w-64 sm:max-w-80 overflow-y-scroll">
+    <p class="tracking-tight text-sm lg:text-base">
       Sempre estive em contato com a tecnologia, seja através de jogos ou simplesmente fazendo buscas na internet, mas nunca fui além disso.
-      Graças a minha curiosidade e vontade de saber o porquê das coisas, cá estou inserida nesse meio tecnológico e inovador.
+      Graças a minha curiosidade e vontade de saber o porquê das coisas, cá estou inserida nesse meio tecnológico e inovador.<br/>
       Iniciei meus estudos sobre desenvolvimento web no final de 2021 e, agora exercendo a profissão, sigo minha jornada progredindo minhas habilidades técnicas e pessoais, além de acompanhar as tendências do mercado.
     </p>
   </div>
